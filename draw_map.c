5bcf98417553da1e3954a7e8db454bf5c4215b2d
Flip <flip@pop-os.localdomain>
2023-01-19 15:45:48 +0100

    NOW ALSO SQUARES! BUG: SOME SLOPES CANNOT BE RENDERED

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: fvan-wij <fvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/16 17:42:53 by fvan-wij      #+#    #+#                 */
-/*   Updated: 2023/01/18 22:58:58 by flip          ########   odam.nl         */
+/*   Updated: 2023/01/19 15:29:09 by flip          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,53 +42,197 @@ void	hook(void *param)
 
 void iso(mlx_image_t* image, int x, int y, Map **map)
 {
-    int previous_x; 
-    int previous_y;
 	int	tileWidth;
 
-    previous_x = x;
-    previous_y = y;
-	tileWidth = 20;
+	tileWidth = 30;
     map[y][x].iso_x = (x - y) * tileWidth + (WIDTH/2);
-    map[y][x].iso_y = (previous_x + y - map[y][x].z) * (tileWidth / 2) + 450;
-	// if ((map[y][x].iso_x  > 0 && map[y][x].iso_x  < WIDTH) && (map[y][x].iso_y  > 0 && map[y][x].iso_y < HEIGHT))
-	// 	mlx_put_pixel(image, map[y][x].iso_x, map[y][x].iso_y, 0xFFFFFFFF);
-	
+    map[y][x].iso_y = (x + y - map[y][x].z) * (tileWidth / 2) + 450;
 }
 
-void	draw_line(mlx_image_t *image, int x0, int y0, int x1, int y1)
+void	draw_omnidirectional_lineX(mlx_image_t *image, int x1, int y1, int x2, int y2)
 {
-	int dx;
-	int dy;
-	int p;
-	int x;
-	int y;
-	
-	dx = x1 - x0;
-	dy = y1 - y0;
+	// Figure out the slope, negative, positive, horizontal or vertical? 
+	int	parametric_value;
+	int m_slope;
+	int	delta_x;
+	int	delta_y;
+	int	x;
+	int	y;
+	int	t;
 
-	x = x0;
-	y = y0;
+	if ((x2 - x1 == 0)) // -> Vertical line
+		m_slope = (y2 - y1);
+	else
+		m_slope = (y2 - y1)/(x2 - x1); //Delta Y over Delta X
 	
-	p = 2 * dy - dx;
- 
-	while(x < x1)
+	if (abs(m_slope) < 1)
 	{
-		// ft_printf("YEET");
-		if(p >= 0)
+		if (x1 > x2) 
 		{
+		t = x1;
+		x1 = x2;
+		x2 = t;
+		t = y1;
+		y1 = y2;
+		y2 = t;
+		}
+		delta_x = abs(x2 - x1);
+		delta_y = abs(y2 - y1);
+		parametric_value = 2 * delta_y - delta_x; // -> This decides whether to increment or stay equal relative to the constant incrementation of either X or Y.
+		x = x1;
+		y = y1;
+		while (x <= x2) // -> Draws points from x1 to x2
+		{
+		
 			if ((x  > 0 && x  < WIDTH) && (y  > 0 && y < HEIGHT))
 				mlx_put_pixel(image, x, y, 0xFFFFFFFF);
-			y = y + 1;
-			p = p + 2 * dy - 2 * dx;
+			x++;
+			if (parametric_value >= 0)
+			{
+				if (m_slope < 1)
+					y++;
+				else
+					y--;
+				parametric_value = parametric_value + 2 * delta_y - 2 * delta_x;
+			}
+			else
+			{
+				y = y;
+				parametric_value = parametric_value + 2  * delta_y;
+			}
 		}
-		else
+	}
+
+	if (abs(m_slope) >= 1)
+	{
+		if (y1 > y2) 
 		{
+			t = x1;
+			x1 = x2;
+			x2 = t;
+			t = y1;
+			y1 = y2;
+			y2 = t;
+		}
+		delta_x = abs(x2 - x1);
+		delta_y = abs(y2 - y1);
+		parametric_value = 2 * delta_x - delta_y; // -> This decides whether to increment or stay equal relative to the constant incrementation of either X or Y.
+		x = x1;
+		y = y1;
+		while (y <= y2) // -> Draws points from x1 to x2
+		{
+		
 			if ((x  > 0 && x  < WIDTH) && (y  > 0 && y < HEIGHT))
 				mlx_put_pixel(image, x, y, 0xFFFFFFFF);
-			p = p + 2 * dy;
+			y++;
+			if (parametric_value >= 0)
+			{
+				if (m_slope >=1)
+					x++;
+				else
+					x--;
+				parametric_value = parametric_value + 2 * delta_x - 2 * delta_y;
+			}
+			else
+			{
+				x = x;
+				parametric_value = parametric_value + 2  * delta_x;
+			}
 		}
-		x = x + 1;
+	}
+}
+
+
+void	draw_omnidirectional_lineY(mlx_image_t *image, int x1, int y1, int x2, int y2)
+{
+	// Figure out the slope, negative, positive, horizontal or vertical? 
+	int	parametric_value;
+	int m_slope;
+	int	delta_x;
+	int	delta_y;
+	int	x;
+	int	y;
+	int	t;
+
+	if ((x2 - x1 == 0)) // -> Vertical line
+		m_slope = (y2 - y1);
+	else
+		m_slope = (y2 - y1)/(x2 - x1); //Delta Y over Delta X
+	
+	if (abs(m_slope) < 1)
+	{
+		if (x1 > x2) 
+		{
+		t = x1;
+		x1 = x2;
+		x2 = t;
+		t = y1;
+		y1 = y2;
+		y2 = t;
+		}
+		delta_x = abs(x2 - x1);
+		delta_y = abs(y2 - y1);
+		parametric_value = 2 * delta_y - delta_x; // -> This decides whether to increment or stay equal relative to the constant incrementation of either X or Y.
+		x = x1;
+		y = y1;
+		while (x <= x2) // -> Draws points from x1 to x2
+		{
+		
+			if ((x  > 0 && x  < WIDTH) && (y  > 0 && y < HEIGHT))
+				mlx_put_pixel(image, x, y, 0xFFFFFFFF);
+			x++;
+			if (parametric_value >= 0)
+			{
+				if (m_slope > 1)
+					y++;
+				else
+					y--;
+				parametric_value = parametric_value + 2 * delta_y - 2 * delta_x;
+			}
+			else
+			{
+				y = y;
+				parametric_value = parametric_value + 2  * delta_y;
+			}
+		}
+	}
+
+	if (abs(m_slope) >= 1)
+	{
+		if (y1 > y2) 
+		{
+			t = x1;
+			x1 = x2;
+			x2 = t;
+			t = y1;
+			y1 = y2;
+			y2 = t;
+		}
+		delta_x = abs(x2 - x1);
+		delta_y = abs(y2 - y1);
+		parametric_value = 2 * delta_x - delta_y; // -> This decides whether to increment or stay equal relative to the constant incrementation of either X or Y.
+		x = x1;
+		y = y1;
+		while (y <= y2) // -> Draws points from x1 to x2
+		{
+		
+			if ((x  > 0 && x  < WIDTH) && (y  > 0 && y < HEIGHT))
+				mlx_put_pixel(image, x, y, 0xFFFFFFFF);
+			y++;
+			if (parametric_value >= 0)
+			{
+				if (m_slope >=1)
+					x++;
+				else
+					x--;
+				parametric_value = parametric_value + 2 * delta_x - 2 * delta_y;
+			}
+			else
+			{
+				x = x;
+				parametric_value = parametric_value + 2  * delta_x;
+			}
+		}
 	}
 }
 
@@ -104,8 +248,10 @@ void	render_map(Map **map, mlx_image_t* image)
 		while (!map[y][x].end_of_row)
 		{
 			iso(image, x, y, map);
-			if (x > 1)
-				draw_line(image, map[y][x - 1].iso_x, map[y][x - 1].iso_y, map[y][x].iso_x, map[y][x].iso_y);
+			if (x >= 1)
+				draw_omnidirectional_lineX(image, map[y][x - 1].iso_x, map[y][x - 1].iso_y, map[y][x].iso_x, map[y][x].iso_y);
+			if (y >= 1)
+				draw_omnidirectional_lineY(image, map[y - 1][x].iso_x, map[y - 1][x].iso_y, map[y][x].iso_x, map[y][x].iso_y);
 			x++;
 		}
 		x = 0;
@@ -122,7 +268,6 @@ int32_t	init_window(Map **map)
 		exit(EXIT_FAILURE);
 	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	render_map(map, g_img);
-	// ft_printf("iso_x = %d\n", map[0][0].iso_x);
 	mlx_image_to_window(mlx, g_img, 0, 0);
 	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
